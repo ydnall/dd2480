e4772bcb1b44d04ce168c8134556c82e64e450b0
Potoqin <142096331+Potoqin@users.noreply.github.com>
2026-01-25 16:41:52 +0100

    Implement LIC 11

---
+++ assignment1/src/launch_interceptor_program/lic/lic_11.py
@@ -0,0 +1,21 @@
+"""
+LIC 11: X decreases over G_PTS points.
+"""
+
+from ..model import Point, Parameters
+
+def lic_11(points: list[Point], params: Parameters) -> bool:
+    n = len(points)
+    if n < 3:
+        return False
+    
+    g_pts = params.G_PTS
+    if g_pts < 1 or g_pts > n - 2:
+        return False
+    
+    for i in range(n - g_pts - 1):
+        j = i + g_pts + 1
+        if points[j][0] < points[i][0]:  # Xj - Xi < 0
+            return True
+    
+    return False
