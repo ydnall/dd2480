4c1f5073fb9f5e0fa67a75867f80598a4b5141b6
Martin Zivojinovic <zivomartin@gmail.com>
2026-01-25 17:25:21 +0100

    refactor: temporarily removing circumcircle_radius before merging.

--- assignment1/src/launch_interceptor_program/lic/lic_1.py
+++ assignment1/src/launch_interceptor_program/lic/lic_1.py
@@ -1,5 +1,4 @@
 from ..model import Parameters, Points
-from .geometry import circumcircle_radius
 
 def lic_1(points: Points, parameters: Parameters) -> bool:
     """
@@ -7,7 +6,8 @@ def lic_1(points: Points, parameters: Parameters) -> bool:
     a circle with radius greater than RADIUS1 to enclose them.
     """
     for (p1, p2, p3) in zip(points, points[1:], points[2:]):
-        if circumcircle_radius(p1, p2, p3) > parameters.RADIUS1:
-            return True
+        # TODO: use the Andy function
+        # if circumcircle_radius(p1, p2, p3) > parameters.RADIUS1:
+        #     return True
 
     return False
